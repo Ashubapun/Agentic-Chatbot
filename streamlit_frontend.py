ec775b7b6590ac7d5c40a890cbfa3b19ce15334b
Abhisek Das <abhisekdas@MacBook-Air.local>
2025-09-04 18:28:53 +0530

    frontend updated

--- streamlit_frontend.py
+++ streamlit_frontend.py
@@ -4,6 +4,7 @@ from langchain_core.messages import BaseMessage, HumanMessage, AIMessage, System
 
 CONFIG = {"configurable" : {"thread_id" : "thread-1"}}
 
+# st.session_state -> dict -> 
 # creating session_state to store the history of messages
 if "message_history" not in st.session_state:
     st.session_state["message_history"] = []
@@ -18,13 +19,17 @@ user_input = st.chat_input("Type here ")
 
 if user_input:
 
+    # first add the message to message_history
     st.session_state["message_history"].append({"role" : "user", "content" : user_input})
     with st.chat_message("user"):
         st.text(user_input)
-
-    response = bot.invoke({"messages" : HumanMessage(content = user_input)}, config = CONFIG)
-    ai_message = response["messages"][-1].content
-    st.session_state["message_history"].append({"role" : "assistant", "content" : ai_message})
-    with st.chat_message("assistant"):
-        st.text(ai_message)
-        
+    
+    with st.chat_message('assistant'):
+        ai_message = st.write_stream(
+            message_chunk.content for message_chunk, metadata in bot.stream(
+                {'messages': [HumanMessage(content=user_input)]},
+                config= {'configurable': {'thread_id': 'thread-1'}},
+                stream_mode= 'messages'
+            )
+        )
+    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
