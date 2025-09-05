c9f26016fdbbea24690b02f98ea0af8bb4cb40fa
Abhisek Das <abhisekdas@MacBook-Air.local>
2025-09-06 02:39:38 +0530

    Sqlite db added

---
+++ db_backend.py
@@ -0,0 +1,60 @@
+import os
+from typing import TypedDict, Annotated, List, Literal
+from langchain_core.messages import BaseMessage, HumanMessage, AIMessage, SystemMessage
+from langchain_groq import ChatGroq
+from langchain_core.tools import tool
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langgraph.prebuilt import create_react_agent
+from langgraph.graph.message import add_messages
+import sqlite3
+from langgraph.checkpoint.serde.encrypted import EncryptedSerializer
+from langgraph.checkpoint.sqlite import SqliteSaver
+from langgraph.graph import StateGraph, END, MessagesState, START
+from langgraph.prebuilt import ToolNode
+
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import HumanMessage
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
+# response = llm.invoke([HumanMessage(content="Hello, how are you?")])
+# print(response.content)
+
+#Define State
+class State(TypedDict):
+    messages : Annotated[list[BaseMessage], add_messages]
+
+# Chatbot Functionality
+def chatbot(state: State):
+    """You are an intelligent chatbot. you have to answer all the questions asked by user and if you don't know answer to any of the question then simply say that you don't know with an apology, don't make up things if you don't know."""
+
+    messages = state["messages"]
+    response = llm.invoke(messages)
+
+    return {"messages":response}
+
+# ------------ Sqlite Db ---------------------
+
+conn = sqlite3.connect(database = "chatbot.db", check_same_thread=False)
+
+checkpointer = SqliteSaver(conn = conn)
+
+# -------------------------------------------
+
+
+# Build StateGraph
+graphbuilder = StateGraph(State)
+
+# add nodes
+graphbuilder.add_node("chatbot", chatbot)
+
+# add edges
+graphbuilder.add_edge(START, "chatbot")
+graphbuilder.add_edge("chatbot", END)
+
+bot = graphbuilder.compile(checkpointer=checkpointer)
+
---
+++ db_frontend.py
@@ -0,0 +1,82 @@
+import streamlit as st
+from backend import bot
+from langchain_core.messages import BaseMessage, HumanMessage, AIMessage, SystemMessage
+import uuid
+from utils.generate_thread import generate_thread_id
+from utils.generate_new_chat import new_chat
+from utils.add_threads import add_thread_id
+from utils.load_chat_history import load_chat
+
+
+# st.session_state -> dict -> 
+# creating session_state to store the history of messages
+if "message_history" not in st.session_state:
+    st.session_state["message_history"] = []
+
+
+if "thread_id" not in st.session_state:
+    st.session_state["thread_id"] = generate_thread_id()
+
+
+if "chat_threads" not in st.session_state:
+    st.session_state["chat_threads"] = []
+
+add_thread_id(st.session_state["thread_id"])
+
+
+# ------------------------------------------------------------
+
+CONFIG = {"configurable" : {"thread_id" : st.session_state["thread_id"]}}
+
+# ----------------- SIDEBAR UI --------------------------------
+ 
+st.sidebar.title("Agentic Chatbot 🤖")
+
+if st.sidebar.button("New Chat"):
+    new_chat()
+
+st.sidebar.header("Chats")
+
+for thread_id in st.session_state["chat_threads"][::-1]:
+    if st.sidebar.button(str(thread_id)):
+        st.session_state["thread_id"] = thread_id
+        messages = load_chat(thread_id)
+
+        temp_messages = []
+
+        for msg in messages:
+            if isinstance(msg, HumanMessage):
+                role='user'
+            else:
+                role='assistant'
+            temp_messages.append({'role': role, 'content': msg.content})
+
+        st.session_state['message_history'] = temp_messages
+
+# ----------------- MAIN UI ----------------------------------
+
+# loading the message history
+for message in st.session_state["message_history"]:
+    with st.chat_message(message["role"]):
+        st.text(message["content"])
+
+
+
+user_input = st.chat_input("Ask Anything ")
+
+if user_input:
+
+    # first add the message to message_history
+    st.session_state["message_history"].append({"role" : "user", "content" : user_input})
+    with st.chat_message("user"):
+        st.text(user_input)
+    
+    with st.chat_message('assistant'):
+        ai_message = st.write_stream(
+            message_chunk.content for message_chunk, metadata in bot.stream(
+                {'messages': [HumanMessage(content=user_input)]},
+                config= CONFIG,
+                stream_mode= 'messages'
+            )
+        )
+    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
